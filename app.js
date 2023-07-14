65e69689d854bcd954d0a01104a1775dfdec1961
KarthikeyanManikandan <138754092+KarthikeyanManikandan@users.noreply.github.com>
2023-07-14 01:52:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function change(){
+    var img=document.getElementById("bulb");
+    if(img.src.match("bulboff")){
+        img.src="./assets/bulbon.jfif";
+
+    }
+
+else{
+img.src="./assets/bulboff.jfif";
+}
+}
